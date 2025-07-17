ccdc6111fe71b8474fa135da8d944bfde77c6c19
nikhilsathish20 <nikhilsathish20@gmail.com>
2025-07-17 13:32:13 +0530

    Created and Uploaded Machine Learning Project

--- heart_failure_prediction/heart_failure_prediction/settings.py
+++ heart_failure_prediction/heart_failure_prediction/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'qggsg9t4!grjyu3sny=n@op19(c&pf(pur@!xist!fje+1_mss'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: list[str] = []
 
 
 # Application definition
